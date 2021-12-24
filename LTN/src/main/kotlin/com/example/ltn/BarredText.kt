49c73e10e3fb58bb54ce06e9906a2f205cc277b3
Nicolò Vescera <nicolo.vescera@gmail.com>
2021-12-24 16:22:22 +0100

    Migliorato BarredText
    Ora capisce quando avere un colore bianco o grigio.
    Migliorato fxml.

--- LTN/src/main/kotlin/com/example/ltn/BarredText.kt
+++ LTN/src/main/kotlin/com/example/ltn/BarredText.kt
@@ -5,23 +5,54 @@ import javafx.scene.control.Label
 import javafx.scene.layout.Pane
 import javafx.scene.shape.Line
 
-class BarredText (private var text:String): StackPane() {
+class BarredText (text:String): StackPane() {
     private val label = Label(text)
     private val panel = Pane()
     private val line = Line()
 
+    // if TRUE, a line appears on the text
+    var isBarred: Boolean
+        get() = line.isVisible
+        set(value) {
+            line.isVisible = value
+        }
+
+    companion object companion {
+        var conta = 0
+
+        fun new() {
+            this.conta ++
+        }
+
+        fun isOdd(): String {
+            return if (conta%2 == 0)
+                "pane_odd"
+            else
+                "pane_even"
+        }
+    }
+
     init {
-        panel.styleClass.add("pane")
+        companion.new()
 
-        line.strokeWidth = 1.5
+        panel.styleClass.add(companion.isOdd())    // set CSS class
 
+        line.strokeWidth = 1.5  // set line stroke
+
+        // binds the line to the label
         line.startXProperty().bind(label.layoutXProperty())
         line.startYProperty().bind(label.layoutYProperty().add(label.heightProperty().multiply(.54)))
         line.endXProperty().bind(label.layoutXProperty().add(label.widthProperty()))
         line.endYProperty().bind(label.layoutYProperty().add(label.heightProperty().multiply(.54)))
 
+        // on spawn text is not barred
+        this.isBarred = false
+
+        // add line to the panel
         panel.children.addAll(line)
 
+        // add panel and label to the StackPane
         super.getChildren().addAll(panel, label)
     }
+
 }
--- LTN/src/main/kotlin/com/example/ltn/HelloController.kt
+++ LTN/src/main/kotlin/com/example/ltn/HelloController.kt
@@ -1,62 +1,35 @@
 package com.example.ltn
 
-import javafx.css.PseudoClass
 import javafx.fxml.FXML
 import javafx.scene.control.Label
 import javafx.scene.control.TextField
 import javafx.scene.input.KeyCode
 import javafx.scene.input.KeyEvent
 import javafx.scene.input.MouseEvent
-import javafx.scene.layout.Pane
-import javafx.scene.layout.StackPane
 import javafx.scene.layout.VBox
-import javafx.scene.shape.Line
 
 
 class HelloController {
-    @FXML private lateinit var welcomeText: Label
     @FXML private lateinit var box:VBox
 
     private val list: ArrayList<TextField> = ArrayList()
-    private lateinit var textField: TextField
+    @FXML private lateinit var textField: TextField
 
+    /*
     @FXML
     fun initialize() {
-        textField = TextField().apply { setOnKeyPressed { onTextTyped(it) } }
+        //textField = TextField().apply { setOnKeyPressed { onTextTyped(it) } }
 
-        box.children.add(textField)
-
-        var label = Label("Io sono una scritta lunga")
-
-        //box.children.add(label)
-        /*
-        val pane = Pane()
-        pane.styleClass.add("pane")
-        val blueLine = Line()
-        blueLine.strokeWidth = 1.5
-        blueLine.startXProperty().bind(label.layoutXProperty())
-        blueLine.startYProperty().bind(label.layoutYProperty().add(label.heightProperty().multiply(.6)))
-        blueLine.endXProperty().bind(label.layoutXProperty().add(label.widthProperty()))
-        blueLine.endYProperty().bind(label.layoutYProperty().add(label.heightProperty().multiply(.6)))
-
-
-        pane.children.addAll(blueLine)
-
-        val aaa = StackPane(pane, label)
-        */
-
-        box.children.add(BarredText("Io sono una stringa bella"))
-
-        //aaa.children[0].isVisible = false
+        //box.children.add(textField)
     }
-
-
-    fun onTextTyped(event: KeyEvent) {
+    */
+    @FXML
+    private fun onTextTyped(event: KeyEvent) {
         if (event.code != null)
             if (event.code == KeyCode.ENTER) {
                 println("ENTER PRESSED !")
                 if (textField.text.isNotEmpty()) {
-                    val tmp = Label(textField.text).apply {
+                    val tmp = BarredText(textField.text).apply {
                         setOnMouseClicked { onMousePressed(it) }
                     }
 
@@ -69,12 +42,11 @@ class HelloController {
             }
     }
 
-    fun onMousePressed(event: MouseEvent) {
+    private fun onMousePressed(event: MouseEvent) {
         println("MousePressed $event")
-        val source = event.source as Label
+        val source = event.source as BarredText
 
-        val withdrawn = PseudoClass.getPseudoClass("withdrawn")
-        source.pseudoClassStateChanged(withdrawn, true);
+        source.isBarred = !source.isBarred
 
     }
 }
